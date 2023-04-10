65ad9f8649535dafd6d059f2738a1b66ef7622ab
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-04-10 12:46:32 -0500

    changed notes.js to intro.js

---
+++ Section 4/Intro.js
@@ -0,0 +1,26 @@
+/*
+Objectives:
+-Define what an algorithm is.
+-Formulate a plan to solve various algorithms.
+-Compare and contrast problem solving patterns which include frequency coutners, two pointer problems, and divide and conquer.
+
+What is an algorithm?
+-The process on accomplishing a task. Or a set of steps that you use to solve a problem.
+
+Why is this important?
+-Practically everything in programming involves algorthms
+-It strengthens the foundation of being a great problem solver and developer.
+-Interviews.
+
+How can you improve?
+-Create a plan for solving a problem.
+-Learn and understsand common problem solving patterns.
+
+Problemm Solving Steps:
+-Understand a problem
+-Explore Concrete Examples
+-Break It Down
+-Solve/Simplify
+-Look Back and Refractor
+
+*/
